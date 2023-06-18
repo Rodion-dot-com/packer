404a7ba896953abf39cd7768a8a46c3a5a37d45c
Родион Прошляков <proshlyakovrodion@yandex.ru>
2023-06-18 13:01:34 +0300

    Updated requirements

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,14 +1,10 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 
-from api.views import PackViewSet, new_pack, select_carton, sku_check
+from api.views import new_pack, select_carton, sku_check
 
-router = DefaultRouter()
-router.register('orders', PackViewSet)
 
 urlpatterns = [
     path('new-order/', new_pack),
     path('sku-check/', sku_check),
     path('selected-carton/', select_carton),
-    path('', include(router.urls)),
 ]
